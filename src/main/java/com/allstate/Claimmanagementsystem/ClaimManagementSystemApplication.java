b730dec771b256176fd74e4b5c0d18236f7a8a8e
Anshu Kumari <anshu.kumari@thoughtworks.com>
2023-06-02 18:26:52 +0530

    Add. Request and Response xml

---
+++ src/main/java/com/allstate/Claimmanagementsystem/ClaimManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.Claimmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/Claimmanagementsystem/ClaimManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.Claimmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
