cf13d53ea4a8d003410da75216179c5f6ddea6d8
twdharani <dharani.thangavel@thoughtworks.com>
2023-06-07 14:56:03 +0530

    Remove rest claim service from soap service

--- src/main/java/com/allstate/soapclaimwebservice/ClaimManagementSystemApplication.java
+++ src/main/java/com/allstate/soapclaimwebservice/ClaimManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.allstate.restclaimwebservice")
 public class ClaimManagementSystemApplication {
 
 	public static void main(String[] args) {
